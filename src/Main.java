9e462ccc960b78ad470d605f5893aae719906772
Zinaida Tian <destinex@yandex.ru>
2022-12-13 17:20:12 +0400

    add src/

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket_price = 150;
+        int oneMile = 20;
+        int bonus = ticket_price / oneMile;
+        System.out.println("Количество бонусных милль " + bonus);
+    }
+}
